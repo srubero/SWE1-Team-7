6af0451d0d369197229b3f1ef693c66016e59686
Haiyutao01 <82407555+Haiyutao01@users.noreply.github.com>
2025-04-15 21:17:38 -0400

    update

--- addition/QRscanner/QRscanner.js
+++ addition/QRscanner/QRscanner.js
@@ -1,22 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Create the QR scanner button
+    
     const QRscannerDiv = document.createElement('div');
     QRscannerDiv.className = 'QRscanner-button-container';
-
-    // Add the QR scanner button to the div
     QRscannerDiv.innerHTML = `
         <button type="button" id="QRscannerButton" class="QRscanner-button">Scan QR</button>
     `;
 
-    // Find the container where you want to add the button
     const formActions = document.querySelector('.form-actions');
     const cancelButton = document.getElementById('cancelAddButton');
     
-    // Insert the QR scanner button before the Cancel button
     formActions.insertBefore(QRscannerDiv, cancelButton);
 
-    // Add event listener for the QR button
     document.getElementById('QRscannerButton').addEventListener('click', () => {
-        window.location.href = '../addition/QRscanner/QRscanner.html'; // Navigate to QR scanner page
+        window.location.href = '../addition/QRscanner/QRscanner.html'; 
     });
 });
